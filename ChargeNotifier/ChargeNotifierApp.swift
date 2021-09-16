01b173fa4064e3110b1f289700f88dc63016b443
Anfernee Viduya <aviduya@apple.com>
2021-09-16 09:33:01 -0500

    Initial Commit

---
+++ ChargeNotifier/ChargeNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargeNotifierApp.swift
+//  ChargeNotifier
+//
+//  Created by Anfernee Viduya on 9/16/21.
+//
+
+import SwiftUI
+
+@main
+struct ChargeNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
